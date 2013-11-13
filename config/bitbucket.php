ccec6e1ad215b839d348691529b91028ebe924b1
Evgeny Golubev <evgeny@golubev.eu>
2013-11-14 01:06:33 +0300

    Add deploy24.com repository

--- config/bitbucket.php
+++ config/bitbucket.php
@@ -4,6 +4,11 @@ return array(
 		'branch' => 'master',
 		'remote' => 'origin',
 		'path' => '/var/www/rusantic.deploy24.com/'
+	),
+	'deploy24.com' => array(
+		'branch' => 'master',
+		'remote' => 'origin',
+		'path' => '/var/www/deploy24.com/'
 	)
 );
 
