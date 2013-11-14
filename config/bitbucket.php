cc3a4e6cccb02128b61bab18fce6b065feec5ad6
Evgeny Golubev <evgeny@golubev.eu>
2013-11-14 03:10:40 +0300

    Fix repo

--- config/bitbucket.php
+++ config/bitbucket.php
@@ -11,7 +11,7 @@ return array(
 		'path' => '/var/www/rusantic.deploy24.com/',
 		'composer' => true
 	),
-	'chatter-demo' => array(
+	'Chatter Demo' => array(
 		'branch' => 'master',
 		'remote' => 'origin',
 		'path' => '/var/www/chatter.deploy24.com/',
