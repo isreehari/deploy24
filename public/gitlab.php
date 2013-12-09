89055c72aa1edff44ef6852c47c0a292e8f5954e
Evgeny Golubev <evgeny@golubev.eu>
2013-12-09 22:07:08 +0300

    Fix gitlab hook handler

--- public/gitlab.php
+++ public/gitlab.php
@@ -5,7 +5,11 @@
 	require_once APP_PATH.'/Deploy/AbstractHook.php';
 	require_once APP_PATH.'/Deploy/GitLabHook.php';
 
-	$repos = require_once APP_PATH.'/config/gitlab.php';
+	$payload = file_get_contents("php://input");
 
-	$hook = new Deploy\GitLabHook($repos);
-	$hook->payload(file_get_contents("php://input"));
+	if (!empty($payload)) {
+		$repos = require_once APP_PATH.'/config/gitlab.php';
+
+		$hook = new Deploy\GitLabHook($repos);
+		$hook->payload();
+	}
