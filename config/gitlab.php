ec27f808bb6b2862d4ae756ee954156f97f22d80
Evgeny Golubev <evgeny@golubev.eu>
2013-12-10 19:53:36 +0300

    Move zakonum to master

--- config/gitlab.php
+++ config/gitlab.php
@@ -30,7 +30,7 @@ return array(
         'submodule' => true
     ),
     'zakonum.ru' => array(
-        'branch' => 'develop',
+        'branch' => 'master',
         'remote' => 'origin',
         'path' => '/var/www/zakonum.deploy24.com/',
         'commands' => array(
