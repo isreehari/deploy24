1649b4326c77f7d9391d7f666b7c3c3d916811ea
Evgeny Golubev <evgeny@golubev.eu>
2014-05-22 14:22:42 +0300

    Update information output

--- Deploy/GitLabHook.php
+++ Deploy/GitLabHook.php
@@ -14,6 +14,7 @@ class GitLabHook extends AbstractHook {
 		$branch = basename($payload->ref);
 		$commit = substr($payload->commits[0]->id, 0, 12);
 		if (isset($this->repos[$name]) && $this->repos[$name]['branch'] === $branch) {
+			$this->log('Hook repository `' . $name . '` on branch `' . $branch . '`');
 			$repo = $this->repos[$name];
 			$repo['commit'] = $commit;
 			$this->process($name, $repo);
